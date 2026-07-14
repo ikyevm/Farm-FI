45641c9ed880bc184e27f67e55706237cfa4ac6e
ikyevm <ikyevm@gmail.com>
2026-07-14 22:20:54 +0700

    Create farmer.js

---
+++ assets/js/farmer.js
@@ -0,0 +1,8 @@
+// =========================================
+// FARM-FI : FARMER KYC LOGIC
+// =========================================
+
+function submitApplication(event) {
+    event.preventDefault();
+    alert("Aplikasi Peternak Berhasil Dikirim!\n\nTim FarmFI akan memverifikasi lahan dan ternak Anda (KYC/KYB) dalam 2x24 jam.");
+}
