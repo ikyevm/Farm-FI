8d6b19b5f4dfdd9dfde9c2fa3143e1e9b2f0a628
ikyevm <ikyevm@gmail.com>
2026-07-14 22:22:49 +0700

    Create sell.js

---
+++ assets/js/sell.js
@@ -0,0 +1,8 @@
+// =========================================
+// FARM-FI : MINT ASSET LOGIC
+// =========================================
+
+function mintAsset(event) {
+    event.preventDefault();
+    alert("Memproses Minting NFT Sapi...\n\nData sedang dikirim ke Smart Contract FarmFI di OPN Testnet.");
+}
