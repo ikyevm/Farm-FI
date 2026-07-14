29c211dfa70394001a49471c9629c59cd828ed21
ikyevm <ikyevm@gmail.com>
2026-07-14 22:19:07 +0700

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// =========================================
+// FARM-FI : GLOBAL APP LOGIC
+// =========================================
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("FarmFI dApp Loaded Successfully!");
+    // Nanti di sini bisa ditambahin logika global lainnya
+});
